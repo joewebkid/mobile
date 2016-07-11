7f256d0045609f707530891cb84b772c1f2f6c2c
Wbook <milkdimka@gmail.com>
2016-07-11 14:37:15 +0300

    i want PUSH2

--- www/lib/ionic-platform-web-client/dist/ionic.io.bundle.js
+++ www/lib/ionic-platform-web-client/dist/ionic.io.bundle.js
@@ -3912,7 +3912,7 @@ var BaseSettings = (function () {
 })();
 
 var temp = new BaseSettings().factory('$ionicCoreSettings', function () {
-  "IONIC_SETTINGS_STRING_START";var settings = {"dev_push":true,"app_id":"9114204a","api_key":"d2b1e512b25b4c6e987b5902209d5b09098f3018f32a08d9","gcm_key":"1091256235296"}; return { get: function(setting) { if (settings[setting]) { return settings[setting]; } return null; } };"IONIC_SETTINGS_STRING_END";
+  "IONIC_SETTINGS_STRING_START";var settings = {"dev_push":false,"app_id":"9114204a","api_key":"d2b1e512b25b4c6e987b5902209d5b09098f3018f32a08d9","gcm_key":"1091256235296"}; return { get: function(setting) { if (settings[setting]) { return settings[setting]; } return null; } };"IONIC_SETTINGS_STRING_END";
 }).finish();
 
 var Settings = (function () {
