e288d8b346f88abceac051a1854253470c9045c7
STEEL_MASTERS <STEEL@MASTERS.RU>
2017-04-27 20:11:16 +0300

    probnaia

---
+++ www/js/links.js
@@ -0,0 +1,13 @@
+window.links = {
+	"ARCADE_URL":"http://work.goodboydigital.com/starwars/arcade/index.html",
+	"GAME1_URL": "http://work.goodboydigital.com/starwars/runner/index.html",
+	"GAME2_URL": "http://work.goodboydigital.com/starwars/rebels/index.html",
+	"GAME3_URL": "http://work.goodboydigital.com/starwars/fighter/index.html",
+
+
+	"ARCADE_URL_MOBILE":"http://work.goodboydigital.com/starwars/arcade/index.html",
+	"GAME1_URL_MOBILE": "http://work.goodboydigital.com/starwars/runner/index.html",
+	"GAME2_URL_MOBILE": "http://work.goodboydigital.com/starwars/rebels/index.html",
+	"GAME3_URL_MOBILE": "http://work.goodboydigital.com/starwars/fighter/index.html"
+
+}
