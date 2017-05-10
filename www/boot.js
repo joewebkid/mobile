4a50e12ae4e7efb38ab704411d4f1de47a12ab3a
STEEL_MASTERS <STEEL@MASTERS.RU>
2017-05-10 13:43:20 +0300

    new

--- www/boot.js
+++ www/boot.js
@@ -1,6 +1,6 @@
 var game = new Phaser.Game(
-    window.innerWidth,
-    window.innerHeight,
+    '100%',
+    '100%',
     Phaser.AUTO,
     ''
     // {
@@ -9,55 +9,59 @@ var game = new Phaser.Game(
     //     update: update
     // }
 );
-var style = { font: '100px "Shark"', fill: '#FFFFFF', stroke: '#000000', strokeThickness: 10, align: 'center' };
-var styleG = { font: '100px "Shark"', fill: '#FFFFFF', stroke: '#000000', strokeThickness: 10, align: 'right' };
+var style = { font: '50px "Shark"', fill: '#FFFFFF', stroke: '#000000', strokeThickness: 10, align: 'center' };
+var styleG = { font: '30px "Shark"', fill: '#FFFFFF', stroke: '#0a73a2', strokeThickness: 5, align: 'right' };
 
 // start boot loader
-var LoadUser = { 
+function fileComplete(progress, cacheKey, success, totalLoaded, totalFiles) {
+    //LoadingText.setText(progress + "%  ");
+    
+    
+    loader_in = game.add.sprite(game.width/2,game.height/2,"loader_in");
+    loader_full = game.add.sprite(game.width/2,game.height/2,"loader_full");    
+    loader_in.anchor.setTo(0.5,0.5);
+    loader_full.anchor.setTo(0.5,0.5);
+    
+    this.load.setPreloadSprite(loader_in,1);
+    this.load.setPreloadSprite(loader_full,0);
+    
+    
+    
+    
+    var txt = LoadingText.setText(progress + "%  ");
+    game.world.bringToTop(txt)
+    
+    
+    console.log(progress);
+    
+    // LoadingText.setText(cacheKey+" Complete: " + progress + "%  ");
+    // LoadingText.setText("File Complete: " + progress + "% - " + totalLoaded + " out of " + totalFiles);
+}
+
+var GameData = { 
     preload: function ()
     {
+        debuger.point('loadCards');
+            card_data = game.cache.getJSON('data');
+            for (key in card_data) {
+                n=key
+                game.load.image(n, card_data[key]['img']);
+                debuger.info(card_data[key]['data']['name']) 
+            }
+        debuger.pointEnd('loadCards');
+
+
         debuger.point('loadUser');
-        var user = MoSql.getOrSet('user',game.cache.getJSON('newUser'))
-        debuger.info(base) 
+            user = MoSql.getOrSet('user',game.cache.getJSON('newUser'))
+            // user = MoSql.set('user',game.cache.getJSON('newUser'))
+            // user = MoSql.get('user')
+            // debuger.info(base) 
         debuger.pointEnd('loadUser');
     },
     create: function () 
     {
-        var tween = game.add.tween(LoadingUser).to({
-            alpha: 0
-        }, 700, Phaser.Easing.Linear.None, true);
-
-        tween.onComplete.add(function() {
-            game.state.start('Menu', false, false);
-        }, this);
-    }
-
-};
-
-var LoadCards = { 
-    preload: function ()
-    {
-        debuger.point('loadCards');
-        var data = game.cache.getJSON('data');
-        for (key in data) {
-            n='b'+key
-            game.load.image(n, data[key]['img']);
-            debuger.info(data[key]['data']['name']) 
-        }
-        debuger.pointEnd('loadCards');
-    },
-    create: function () 
-    {
-        var tween = game.add.tween(LoadingTextCards).to({
-            alpha: 0
-        }, 700, Phaser.Easing.Linear.None, true);
-
-
-        tween.onComplete.add(function() {
-            LoadingUser = game.add.text(game.world.width / 2, game.world.height / 2, "Loading user info", style);
-            game.state.start('LoadUser', false, false);
-            LoadingUser.anchor.setTo(0.5, 0.5);
-        }, this);
+        game.load.onFileComplete.add(fileComplete, this);
+        game.state.start('Menu', false, false);
     }
 
 };
@@ -66,32 +70,46 @@ var Preloader = {
     preload: function ()
     {
         debuger.point('loadAssets');
-        game.load.image('start', './assets/menu/butt.png'); 
-        game.load.json('data', 'assets/game/cards/data.json');
-        game.load.json('newUser', 'assets/game/cards/newUser.json');
+        
+            game.load.image('start', './assets/menu/butt3.png');  
+            game.load.image('setting', './assets/menu/butt_sett.png'); 
+            game.load.image('cards', './assets/menu/butt_cards.png'); 
+            game.load.image('time', './assets/menu/time.png');
+            game.load.image('gold', './assets/menu/gold.png');
+
+            game.load.image('back', './assets/menu/back.jpg'); 
+
+            game.load.image('game_desc', 'assets/game/cards/game_desc2.png');
+            game.load.image('card_desc', 'assets/game/cards/cards_desc.png');
+            
+            game.load.json('data', 'assets/game/cards/data.json');
+            game.load.json('newUser', 'assets/game/cards/newUser.json');
+            
         debuger.pointEnd('loadAssets');
     },
     create: function () 
     { 
-        var tween = game.add.tween(LoadingText).to({
-            alpha: 0
-        }, 700, Phaser.Easing.Linear.None, true);
-
-        tween.onComplete.add(function() {
-
-            LoadingTextCards = game.add.text(game.world.width / 2, game.world.height / 2, "Loading cards", style);
-            game.state.start('LoadCards', false, false);
-            LoadingTextCards.anchor.setTo(0.5, 0.5);
-        }, this);
+        game.load.onFileComplete.add(fileComplete, this);
+        game.state.start('GameData', false, false);
     }
 };
 
 var Boot = { 
+    preload: function ()
+    {
+        game.load.image('loader_in', './assets/menu/loader_in.png');
+        game.load.image('loader_full', './assets/menu/loader_full.png');
+    },
     create: function () 
-    { 
+    {
+
+        game.load.onFileComplete.add(fileComplete, this);
+
         LoadingText = game.add.text(game.world.width / 2, game.world.height / 2, "...", style);
         LoadingText.anchor.setTo(0.5, 0.5);
+        
         game.state.start('Preloader', false, false);
     }
+
 };
 // end boot loader
--- www/game.js
+++ www/game.js
@@ -3,45 +3,183 @@ var Menu =
 { 
     preload: function () 
     { 
+            game.load.image('quake', './assets/game/quake.png');             
+            game.load.image('blanc', './assets/menu/blanc.png');             
+            game.load.image('lvl', './assets/menu/level.png');             
+            game.load.image('koloda', './assets/menu/koloda.png');             
+            game.load.audio('win', ['./assets/game/music/win.mp3']);
+            game.load.audio('clash', ['./assets/game/music/clash.mp3']);
+            game.load.audio('back', ['./assets/game/music/back.mp3']);
+            game.load.audio('ingame', ['./assets/game/music/game.mp3']);
+
+            styleGold = { font: '30px "Shark"', fill: '#FFFFFF', stroke: '#cd7c3f', strokeThickness: 5, align: 'right' };
+            styleBig = { font: '30px "Shark"', fill: '#FFFFFF', stroke: '#111111', strokeThickness: 5, align: 'right' };
+
+        gameMem = MoSql.getOrSet('game',{
+            'player':{
+                'cards':user['cards'],
+                'cardPlace':[],
+                'hp':user['hp'],
+            }
+            ,
+            'enemy':{
+                'cards':[],
+                'hp':user['hp'],
+            }
+            ,
+            'game':{
+                'cards':[]
+            }
+        })
     }, 
     create: function () 
     {         
         Menu.physics.startSystem(Phaser.Physics.ARCADE);
-        Menu.stage.backgroundColor = '#c7996c';
 
-        button = this.add.button(window.innerWidth/2, window.innerHeight/2, 'start', this.startGame, this); 
-        button.anchor.setTo(0.5, 0.5);
+        back_music = game.add.audio('back');
+        ingame = game.add.audio('ingame');
+        back_music.play();
+        back_music.loopFull(0.6);
 
-        buttonText = game.add.text(game.world.width / 2, game.world.height / 2, "Game", style);
-        buttonText.anchor.setTo(0.5, 0.5);
+        bg = this.add.sprite(0, 0, 'back');
+        he=bg.height
+        bg.height = game.height;
+        bg.width = (bg.height/he)*bg.width;
+
+        var lvl = this.add.sprite(0, 50, 'lvl');
+        lvl.anchor.setTo(0, 0.5);
+        lvlT = game.add.text(40, lvl.position.y-5, user.lvl, styleG)
+        lvlT.anchor.setTo(lvl.anchor.x, lvl.anchor.y);
+        lvlT = game.add.text(130, lvl.position.y-5, user.exp, styleG)
+        lvlT.anchor.setTo(lvl.anchor.x, lvl.anchor.y);
+
+        
+
+        var gold = this.add.sprite(lvl.width+20,50 , 'gold');
+        gold.anchor.setTo(0, 0.5);
+        goldT = game.add.text( gold.position.x+130, gold.position.y-5, user.golds, styleGold)
+        goldT.anchor.setTo(gold.anchor.x, gold.anchor.y);
+
+        button = this.add.button(window.innerWidth/2, window.innerHeight/2, 'start', this.startGame, this);
+        button.anchor.setTo(0.5, 0.5); 
+
+        setting = this.add.button(window.innerWidth-247, window.innerHeight-121, 'setting', this.setting, this); 
+        cards = this.add.button(0, window.innerHeight-121, 'cards', this.cards, this); 
+
+        // buttonText = game.add.text(game.world.width / 2, game.world.height / 2, "Game", style);
+        // buttonText.anchor.setTo(0.5, 0.5);
     }, 
     startGame: function () 
     { 
-        this.state.start('Game'); 
+        // var tween = Menu.add.tween(button.scale).to({y:game.height - 200},0, Phaser.Easing.Bounce.Out, true);
+        // tween.onComplete.add(function() {})
+        game.state.start('Game'); 
+        
+    } ,
+    setting: function () 
+    { 
+        
+    } ,
+    cards: function () 
+    { 
+        
     } 
 };
 
 var tween = null;
 cardsArr=[];
+ENEMY=[];
 
 var Game = { 
     preload: function ()
-    {
+    {        
+        game.physics.startSystem(Phaser.Physics.ARCADE)
+        var margin = 50;
+        // and set the world's bounds according to the given margin
+        var x = -margin;
+        var y = -margin;
+        var w = game.world.width + margin * 2;
+        var h = game.world.height + margin * 2;
+        // it's not necessary to increase height, we do it to keep uniformity
+        game.world.setBounds(x, y, w, h);
+        game.world.camera.position.set(0);
+        // 
+        game.stage.backgroundColor = "#dec19f"
+
+        clash = game.add.audio('clash');
+        back_music.stop();
+        ingame.play();
+
+        // bg = this.add.sprite(0, 0, 'back');
+        // he=bg.height
+        // bg.height = game.height;
+        // bg.width = (bg.height/he)*bg.width;
+
+        time = this.add.sprite(window.innerWidth-250, 50, 'time');
+        time.anchor.setTo(0.5, 0.5);
+
+        game_desc = this.add.sprite(window.innerWidth/2, window.innerHeight/2, 'game_desc');
+        game_desc.anchor.setTo(0.5, 0.5);
+// ENEMY
+        for (var key in card_data) {
+            ENEMY[key] = game.add.sprite(window.innerWidth/2, window.innerHeight/4,key)
+            ENEMY[key].anchor.setTo(0.5, 0.5)
+            ENEMY[key].visible = 0
+        }
+        EnemyPosition=ENEMY['b1'].position.y 
+// ENEMY END
+        bigText = game.add.text(window.innerWidth/2, 50, "...", styleBig)
+        bigText.anchor.setTo(0.5, 0.5);
+        bigText.alpha = 0; game.world.bringToTop(bigText)
+
+// 
+
+        koloda = game.add.sprite(0, 0,'koloda')
+        koloda.position.copyFrom(ENEMY['b1'].position);
+        koloda.anchor.setTo(0.5, 0.5);
+        game.world.sendToBack(koloda)
+        game.world.moveUp(koloda)
+
+        quake = game.add.sprite(0, 0,'quake')
+        quake.position.copyFrom(game_desc.position);
+        quake.anchor.setTo(0.5, 0.5);quake.visible = 0
+
+        quake2 = game.add.sprite(0, 0,'quake')
+        quake2.position.copyFrom(ENEMY['b1'].position);
+        quake2.anchor.setTo(0.5, 0.5);quake2.visible = 0
+
+        card_desc = this.add.sprite( window.innerWidth-247, window.innerHeight-121, 'card_desc');
+        card_desc.anchor.setTo(0.5, 0.5);
+
+        game.physics.enable(card_desc, Phaser.Physics.ARCADE);
+        game.physics.enable(game_desc, Phaser.Physics.ARCADE);
     }, 
     create: function () 
     { 
-        total = 10
+        total = 5
         cards = Game.add.group();
         cards.inputEnableChildren = true;
 
-        for (var i = 3; i > 0; i--) {
-            cardsArr[i] = cards.create(100*i, 100, 'b'+i);
-            cardsArr[i].input.enableDrag(false, true);
+        for (var key in card_data) {
+            cardsArr[key] = cards.create(100*key[1], 100, key);
+            cardsArr[key].input.enableDrag(false, true);
 
-            cardsArr[i].events.onDragStart.add(this.onDragStart, this);
-            cardsArr[i].events.onDragStop.add(this.onDragStop, this)
-            cardsArr[i].scale.set(1.9);
+            cardsArr[key].events.onDragStart.add(this.onDragStart, this);
+            cardsArr[key].events.onDragStop.add(this.onDragStop, this)
+            cardsArr[key].scale.set(1.1);
+            cardsArr[key].scale.set(1);
+// card mem
+            Card.add(cardsArr[key].key,gameMem.player.cards,gameMem.player.cardPlace)
+// card position
+            cardsArr[key].position.copyFrom(card_desc.position); 
+            cardsArr[key].anchor.setTo((gameMem.player.cardPlace[key]-1)-card_desc.anchor.x, card_desc.anchor.y); 
+// card position
+            game.physics.enable(cardsArr[key], Phaser.Physics.ARCADE);
         }
+
+        // ENEMY = cards.create(window.innerWidth/2, window.innerHeight/4, key);
+        // ENEMY.anchor.setTo(0.5, 0.5)
+        // EnemyPosition=ENEMY.position.y
 
         timer = game.time.create(false);
         timer.loop(1000, this.updateCounter, this);
@@ -50,47 +188,218 @@ var Game = {
 
         // this.add.button(0, 0, 'back', this.inMenu, this);
         t = game.add.text(window.innerWidth-20, 0, 30, styleG)
-        t.anchor.setTo(1, 0); 
+        t.position.copyFrom(time.position);
+        t.anchor.setTo(time.anchor.x/4, time.anchor.y);
+
+        health_bar = game.add.text(0, window.innerHeight-50, 1, styleG)
+        health_bar_enemy = game.add.text(0, 0, 1, styleG)
     }, 
     update: function () 
     { 
+        
+        
     },
     updateCounter: function () {
-        if(total!=0)
+        if(total!=0){
             total--;
-        else
-            total=10
+        }else{
+            l=gameMem.game.cards.length
+            YOUR_CARD=gameMem.game.cards
+            if(l){
+
+                enemyCardNum=this.randomInteger(1,4)
+                yourType=card_data[YOUR_CARD[0]]['data'].type
+                yourAttack=card_data[YOUR_CARD[0]]['data'].attack
+
+                enemType=card_data['b'+enemyCardNum]['data'].type
+                enemAttack=card_data['b'+enemyCardNum]['data'].attack
+
+                EnemyCard=ENEMY['b'+enemyCardNum]
+                EnemyCard.visible=1;
+                EnemyCard.inputEnabled = true;
+                EnemyCard.input.enableDrag(false, true);
+
+                bigText.alpha = 1
+
+                if(yourType[0]==enemType[0]){
+                    bigText.text ="Ничья"
+                    this.knock(EnemyCard,cardsArr[YOUR_CARD[0] ],'enemy')
+                    this.knock(cardsArr[YOUR_CARD[0] ],EnemyCard)
+
+                    gameMem.player.hp-=enemAttack
+                    gameMem.enemy.hp-=yourAttack
+                }else if(yourType[1]==enemType[0]){
+                    bigText.text ="Победа за вами"
+                    this.knock(cardsArr[YOUR_CARD[0] ],EnemyCard)
+                    gameMem.enemy.hp-=yourAttack
+                }else{
+                    bigText.text ="В этот раз не повезло"
+                    gameMem.player.hp-=enemAttack
+                    this.knock(EnemyCard,cardsArr[YOUR_CARD[0] ],'enemy')
+                }
+                // this.knock(cardsArr[YOUR_CARD[0][1]],ENEMY)
+
+
+            }
+            total=5
+
+        }
     },
     render: function () 
     { 
         var text = timer.duration.toFixed(0);
         t.text=total
+
+        health_bar.text=gameMem.player.hp
+        health_bar_enemy.text=gameMem.enemy.hp
     }, 
     onDragStart: function(obj) {
 
-        if ((tween !== null && tween.isRunning) || obj.scale.x === 1)
+        if ((tween !== null && tween.isRunning) || obj.scale.x === 1.2)
         {
             return;
         }
-        tween = Game.add.tween(obj.scale).to( { x: 3, y: 3 }, 10, Phaser.Easing.Elastic.Out, true);
+        // Phaser.Easing.Elastic.Out
+        tween = Game.add.tween(obj.scale).to( { x: 1.2, y: 1.2 }, 100, Phaser.Easing.Linear.None, true);
     },
     nextTurn: function (obj) {
         this.state.start('Game'); 
     },
+    randomInteger: function (min, max) {
+        var rand = min + Math.random() * (max + 1 - min);
+        rand = Math.floor(rand);
+        return rand;
+    },
     onDragStop: function (obj) {
-        if (tween && tween.isRunning || obj.scale.x === 2)
+        if (tween && tween.isRunning || obj.scale.x === 1)
         {
             return;
         }
-        tween = Game.add.tween(obj.scale).to( { x: 2, y: 2 }, 10, Phaser.Easing.Elastic.In, true);
+        tween = Game.add.tween(obj.scale).to( { x: 1, y: 1 }, 100, Phaser.Easing.Linear.None, true);
+
+
+        game.physics.arcade.collide(obj, card_desc)
+        if (!game.physics.arcade.overlap(obj, card_desc, function() {
+            
+            Card.add(obj.key,gameMem.player.cards,gameMem.player.cardPlace)
+
+            obj.position.copyFrom(card_desc.position); 
+            obj.anchor.setTo((gameMem.player.cardPlace[obj.key]-1)-card_desc.anchor.x, card_desc.anchor.y); 
+        }))
+        { 
+            Card.remove(obj.key,gameMem.player.cards,gameMem.player.cardPlace)
+            obj.position.copyFrom(card_desc.position); 
+            obj.anchor.setTo(card_desc.anchor.x, card_desc.anchor.y); 
+        }
+
+        game.physics.arcade.collide(obj, game_desc)
+        if (!game.physics.arcade.overlap(obj, game_desc, function() {
+
+            l=gameMem.game.cards.length
+            if(!l){
+                Card.add(obj.key,gameMem.game.cards,gameMem.player.cardPlace) 
+                obj.input.draggable = false;
+                obj.position.copyFrom(game_desc.position); 
+                obj.anchor.setTo(game_desc.anchor.x, game_desc.anchor.y); 
+            }
+        }))
+        { }
+
+    },
+    knock:function(elem,enemy,whose){
+
+        scale_tween = game.add.tween(elem.scale).to({
+             x: 1.8, y: 1.8
+        }, 500, Phaser.Easing.Linear.None, true);
+        game.world.bringToTop(elem)
+            // obj.position.copyFrom(card_desc.position); 
+            // obj.anchor.setTo(card_desc.anchor.x, card_desc.anchor.y); 
+        var tween2 = game.add.tween(elem).to({
+             y: enemy.position.y
+        }, 700, Phaser.Easing.Linear.None, true);
+
+
+        tween2.onComplete.add(function(elem) {
+
+            var tween3 = game.add.tween(elem.scale).to({
+             x: 1, y: 1
+            }, 200, Phaser.Easing.Bounce.InOut, true);
+clash.play();
+            tween3.onComplete.add(function() {  
+                if(whose!='enemy'){quake2.visible = 1}
+                else{quake.visible = 1}                
+                var tween4 = game.add.tween(game.camera).to({
+                    x: game.camera.x - 10
+                },10, Phaser.Easing.Bounce.InOut, true,300,4,true);
+// addQuake()
+// console.log()
+                tween4.onComplete.add(function() {
+                    if(whose!='enemy'){
+                        // position
+                        elem.position.copyFrom(0,card_desc.position.y); 
+                        // position
+                        
+                        elem.anchor.setTo(card_desc.anchor.x, card_desc.anchor.y); 
+                        elem.input.enableDrag(false, true);                        
+                        Card.remove(elem.key,gameMem.game.cards,gameMem.player.cardPlace)
+                        enemy.visible = 0
+                    }else{
+                        enemy.position.copyFrom(0,card_desc.position.y); 
+                        enemy.anchor.setTo(card_desc.anchor.x, card_desc.anchor.y); 
+                        enemy.input.enableDrag(false, true);                        
+                        Card.remove(enemy.key,gameMem.game.cards,gameMem.player.cardPlace)
+
+                        elem.position.y=EnemyPosition;                   
+                        elem.visible = 0
+                        quake.visible = 0
+                        quake2.visible = 0
+                        // elem.anchor.setTo(card_desc.anchor.x, card_desc.anchor.y); 
+
+                    }
+                        quake.visible = 0
+                        quake2.visible = 0
+                    game.world.sendToBack(elem)
+                    game.world.moveUp(elem)
+
+                    if(gameMem.player.hp<=0 || gameMem.enemy.hp<=0){
+                        // LOOSE
+                        game.state.start('WIN'); 
+                    }
+                })
+            })
+        })
+
     }
+
 };
 
-game.state.add('LoadUser', LoadUser, false);
-game.state.add('LoadCards', LoadCards, false);
+var WIN = { 
+    preload: function (){},
+    create: function (){
+        ingame.stop();
+        user.golds+=15
+        user.exp+=15
+        MoSql.set('user',user)
+
+        LoadingText = game.add.text(game.world.width / 2, game.world.height / 2, "You win (or lose XD)", style);
+        LoadingText.anchor.setTo(0.5, 0.5);
+        button = this.add.button(window.innerWidth/2, window.innerHeight/2, 'blanc', this.inMenu, this);
+        button.anchor.setTo(0.5, 0.5); 
+        // LoadingText.inputEnabled = true;
+        // LoadingText.events.onInputDown.add(game.state.start('Menu'), this);
+
+        win = game.add.audio('win');
+        win.play();
+
+    },
+    inMenu: function (){game.state.start('Menu');},
+}
+
+game.state.add('GameData', GameData, false);
 game.state.add('Preloader', Preloader, false);
 game.state.add('Boot', Boot, false);
 game.state.add('Menu', Menu); 
+game.state.add('WIN', WIN); 
 
 game.state.add('Game', Game); 
 game.state.start('Boot');
